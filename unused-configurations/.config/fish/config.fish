c65afe1d8cffd420b18c8ee10fabbd16d2d0436e
Icy_Thought <dracaphallus@gmail.com>
2020-06-23 10:25:00 +0200

    Updated configurations to more recent configs.

--- unused-configurations/.config/fish/config.fish
+++ unused-configurations/.config/fish/config.fish
@@ -1,2 +1,9 @@
+# General Configruations
+set fish_greeting                           # Empty greeting message
 set EDITOR "nvim"
 set PATH $HOME/.cargo/bin $PATH
+starship init fish | source
+
+# Aliases
+alias ls="ls -l --color=auto"
+alias SysUpd="sudo dnf update && rustup update && rustup self update && flatpak update && betterdiscordctl update --flatpak"
